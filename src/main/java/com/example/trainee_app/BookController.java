05853e188e9dc4969d372c9d147bb864bcadad5c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-03 09:36:55 +0400

    create a public method that returns a text string and map it to the web path /add-book + Set up the method's input parameters so that it specifically expects an integer ID nd a string Name to be sent from the browser + Create new instance of Book

--- src/main/java/com/example/trainee_app/BookController.java
+++ src/main/java/com/example/trainee_app/BookController.java
@@ -1,5 +1,7 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,4 +10,11 @@ import java.util.List;
 @RestController
 public class BookController {
     private static List<Book> books = new ArrayList<>();
+
+    @GetMapping("/add-book")
+    public String addBook(@RequestParam int id, @RequestParam String name){
+        Book book = new Book(id,name);
+
+        return "Book added successfully!";
+    }
 }
