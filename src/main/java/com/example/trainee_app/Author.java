42e63512e99f1272e146e013558b3b30a83cba19
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-03 11:28:57 +0400

    Generate constructor, getters, and setters.

--- src/main/java/com/example/trainee_app/Author.java
+++ src/main/java/com/example/trainee_app/Author.java
@@ -6,5 +6,37 @@ public class Author {
     private String name;
     private String biography;
 
+    // Constructor
+    public Author(int id, String name, String biography) {
+        this.id = id;
+        this.name = name;
+        this.biography = biography;
 
+    }
+
+    //Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
 }
