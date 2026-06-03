a7140a1faf1fac76332ea9c4026884171273e740
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-03 11:47:17 +0400

    Create a basic GET endpoint mapped to /addAuthor that takes an ID, name, and biography via request parameters, instantiates an Author +  instantiates an Author, saves them to the author list +  returns a success confirmation message

--- src/main/java/com/example/trainee_app/LibraryController.java
+++ src/main/java/com/example/trainee_app/LibraryController.java
@@ -1,5 +1,7 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,5 +13,13 @@ public class LibraryController {
     private static List<Author> authorList = new ArrayList<>();
     private static List<Book> bookList = new ArrayList<>();
 
+    @GetMapping("/addAuthor")
+    public String addAuthor(@RequestParam int id,@RequestParam String name, @RequestParam String biography){
+
+        Author author = new Author(id,name,biography);
+        authorList.add(author);
+
+        return "Author added successfully";
+    }
 
 }
