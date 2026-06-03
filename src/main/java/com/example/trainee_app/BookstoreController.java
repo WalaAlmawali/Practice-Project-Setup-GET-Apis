492411fa9c9dc5d964722e7672f760d424e580fc
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-03 14:34:51 +0400

    Map a GET endpoint that accepts request parameters for the ID, title, price, and stock count + Create a new InventoryBook, add it to the list, and return a confirmation message

--- src/main/java/com/example/trainee_app/BookstoreController.java
+++ src/main/java/com/example/trainee_app/BookstoreController.java
@@ -1,5 +1,8 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,6 +12,15 @@ import java.util.List;
 public class BookstoreController {
 
     private static List<InventoryBook> catalog = new ArrayList<>();
+
+    @GetMapping("/addBook")
+    public String addBook(@RequestParam int id, @RequestParam String title, @RequestParam double price, @RequestParam int stockCount) {
+
+        InventoryBook inventoryBook = new InventoryBook(id, title, price, stockCount);
+        catalog.add(inventoryBook);
+
+        return "Book added successfully to the bookstore catalog";
+    }
 
 
 }
