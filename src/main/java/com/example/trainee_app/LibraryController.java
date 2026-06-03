eef1c01e3c377fc73cb1db41790c27b159804ba6
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-03 11:39:44 +0400

    declare two separate, private, static storage array lists:one dedicated to holding Author objects and one for Book objects

--- src/main/java/com/example/trainee_app/LibraryController.java
+++ src/main/java/com/example/trainee_app/LibraryController.java
@@ -2,6 +2,14 @@ package com.example.trainee_app;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LibraryController {
+
+    private static List<Author> authorList = new ArrayList<>();
+    private static List<Book> bookList = new ArrayList<>();
+
+
 }
