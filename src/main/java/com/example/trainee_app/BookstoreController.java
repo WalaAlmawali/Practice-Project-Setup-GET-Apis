68d056b2a199eb932487382a184f454b04a6c73b
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-03 14:56:39 +0400

    Map a GET endpoint /checkStock + Use a request parameter to capture the target book ID

--- src/main/java/com/example/trainee_app/BookstoreController.java
+++ src/main/java/com/example/trainee_app/BookstoreController.java
@@ -13,13 +13,27 @@ public class BookstoreController {
 
     private static List<InventoryBook> catalog = new ArrayList<>();
 
-    @GetMapping("/addBook")
-    public String addBook(@RequestParam int id, @RequestParam String title, @RequestParam double price, @RequestParam int stockCount) {
+    @GetMapping("/addBookToCatalog")
+    public String addBookToCatalog(@RequestParam int id, @RequestParam String title, @RequestParam double price, @RequestParam int stockCount) {
 
         InventoryBook inventoryBook = new InventoryBook(id, title, price, stockCount);
         catalog.add(inventoryBook);
 
         return "Book added successfully to the bookstore catalog";
+    }
+
+    @GetMapping("/checkStock")
+    public String checkStock(@RequestParam int id) {
+        for (InventoryBook book : catalog) {
+            if (book.getBookId() == id) {
+                if (book.getStockCount() > 0) {
+                    return "Book Available <br>" + "Title" + book.getTitle() + "<br>" + "Price" + book.getPrice();
+                } else {
+                    return "Sold Out <br>" + book.getTitle();
+                }
+            }
+        }
+        return "This book is not exist in bookstore";
     }
 
 
